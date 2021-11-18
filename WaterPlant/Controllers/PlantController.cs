01ace9f3254e08e9be880ce55e2bc45e6fcf899b
harshpanchal99 <40739323+harshpanchal99@users.noreply.github.com>
2021-11-17 20:19:08 -0500

    Add files via upload

---
+++ WaterPlant/Controllers/PlantController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using WaterPlant.Models;
+
+namespace WaterPlant.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlantController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public PlantController(IConfiguration configuration) {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public JsonResult Get() {
+            Console.Write("Here Here");
+            string query = @"select * from plants";
+            Console.Write("Here Here");
+            DataTable table = new DataTable();
+            Console.Write("Here Here");
+            string sqlDataSource = _configuration.GetConnectionString("PlantAppConnection");
+            MySqlDataReader myReader;
+            using (MySqlConnection mycon = new MySqlConnection(sqlDataSource)) {
+                mycon.Open();
+                using (MySqlCommand myCommand = new MySqlCommand(query, mycon)) {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    mycon.Close();
+                }
+            }
+            return new JsonResult(table);
+        }
+
+        [HttpPut]
+        public JsonResult Put(Plant plant)
+        {
+            string query = @"update plants set status = @status,
+                            last_watered='" + DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss") + "' where id=@id";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("PlantAppConnection");
+            MySqlDataReader myReader;
+            using (MySqlConnection mycon = new MySqlConnection(sqlDataSource))
+            {
+                mycon.Open();
+                using (MySqlCommand myCommand = new MySqlCommand(query, mycon))
+                {
+                    myCommand.Parameters.AddWithValue("@status", plant.status);
+                    myCommand.Parameters.AddWithValue("@id", plant.id);
+
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    mycon.Close();
+                }
+            }
+
+            return new JsonResult("Updated Successfully");
+            
+        }
+
+
+    }
+}
---
+++ WaterPlant/Models/Plant.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WaterPlant.Models
+{
+    public class Plant
+    {
+        public int id { get; set; }
+        public string plant_name { get; set; }
+        public int status { get; set; }
+        public string last_watered { get; set; }
+    }
+}
